a90f61e970120920d87a309152d4469b7bef8c74
Nobel <hollow.sky2@gmail.com>
2019-03-17 12:17:43 +0545

    Create server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("hello world");
+
+const http = require("http");
+
+const server = http.createServer(function (request,response) {
+	response.end("hello world");
+})
+
+server.listen(300,function(){
+	console.log("server is running");
+})
